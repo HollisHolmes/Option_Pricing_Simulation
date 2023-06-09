d17d97c47f53a4111ab993b7fc6c6205e8434d07
Monir Imamverdi <monir.imamverdi@gmail.com>
2023-06-09 02:24:01 -0600

    Created header file for price generator class

---
+++ PriceGenerator.h
@@ -0,0 +1,21 @@
+#ifndef EQUITY_PRICE_GENERATOR_H
+#define EQUITY_PRICE_GENERATOR_H
+
+#include<vector>
+
+class PriceGenerator
+{
+
+public:
+	PriceGenerator(double initEquityPrice, unsigned numTimeStamps, double timeToExpiry, double drift, double volatility);
+	std::vector<double> operator()(int seed) const
+
+private:
+	double dt_; 
+	const double initEquityPrice_;
+	const int numTimeSteps_;
+	const double drift_;
+	const double volatility_;
+};
+
+#endif
